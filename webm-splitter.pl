353c1bcb488d3ec74869f12d31d91ffc72d8f27f
Ian Bradley <marlen.crabapple@gmail.com>
2014-12-23 13:44:35 -0800

    Handle command line args with Getopt::Long; New options

--- webm-splitter.pl
+++ webm-splitter.pl
@@ -3,14 +3,31 @@
 use strict;
 
 use JSON;
+use Getopt::Long;
 use File::Basename;
 
-# get input and args
-my $input = $ARGV[0] or die "No input video.\n";
-my $cliplen = $ARGV[1] || 30;
-my $offset = $ARGV[2] || 0;
-my $bitrate = $ARGV[3] || "1M";
-my $crf = $ARGV[4] || 10;
+my ($input, $cliplen, $offset, $stopat, $bitrate, $crf, $vf, $ffmpegargs);
+
+# set defaults
+$cliplen = 30;
+$offset = 0;
+$bitrate = "1M";
+$crf = 10;
+
+# handle command line args
+GetOptions(
+  'input|i=s' => \$input,
+  'cliplength|l=i' => \$cliplen,
+  'offset|startat|o=i' => \$offset,
+  'stopat|roffset|s=i' => \$stopat,
+  'bitrate|b=s' => \$bitrate,
+  'crf|q=i' => \$crf,
+  'vfoptions|vf=s' => \$vf,
+  'additionalargs|ffmpegargs|cl|opts=s' => \$ffmpegargs
+);
+
+die "No input provided." unless $input;
+$vf = "-vf $vf" if $vf;
 
 # get clean filename, path, etc.
 my ($filename, $path, $suffix) = fileparse($input);
@@ -18,10 +35,12 @@ my $input_clean = $path . $filename;
 
 # get video duration
 my $ffprobe_out = decode_json(`ffprobe -v quiet -print_format json -show_format -show_streams '$input_clean'`);
-my $length = int($$ffprobe_out{format}->{duration});
+my $length = $stopat || int($$ffprobe_out{format}->{duration});
 
 # split and encode
 for(my $i = $offset; $i < $length; $i += $cliplen) {
-  my $ffmpegcmd = "ffmpeg -ss $i -i '$input_clean' -t $cliplen -c:v libvpx -crf $crf -b:v $bitrate -an '" . $filename . "_$i.webm'";
-  my $ffmpegout = `$ffmpegcmd`;
+  my $ffmpegcmd = "ffmpeg -ss $i -i '$input_clean' -t $cliplen -c:v libvpx -crf $crf -b:v $bitrate -an $vf $ffmpegargs '" . $filename . "_$i.webm'";
+  print "$ffmpegcmd:\n\n";
+  
+  `$ffmpegcmd`;
 }
