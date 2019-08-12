d17b5787202a0e0a63f22a241eea4b2153eaf03b
Ian P Bradley <ian.bradley@studiocrabapple.com>
2019-08-12 14:53:41 -0700

    Fixed bugs, added 2 pass mode

---
+++ cpanfile
@@ -0,0 +1,6 @@
+requires 'JSON::MaybeXS';
+requires 'Cpanel::JSON::XS';
+requires 'Getopt::Long';
+requires 'File::Basename';
+requires 'Syntax::Keyword::Try';
+requires 'Data::Printer'
--- webm-splitter.pl
+++ webm-splitter.pl
@@ -1,12 +1,17 @@
 #!/usr/bin/perl
 
 use strict;
+use warnings;
 
-use JSON;
+use feature 'say';
+
 use Getopt::Long;
+use Data::Printer;
+use JSON::MaybeXS;
 use File::Basename;
+use Syntax::Keyword::Try;
 
-my ($input, $cliplen, $offset, $stopat, $bitrate, $crf, $vf, $ffmpegargs, $audio);
+my ($input, $cliplen, $offset, $stopat, $bitrate, $crf, $vf, $audio, $two_pass, $ffmpegargs);
 
 # set defaults
 $cliplen = 30;
@@ -23,28 +28,54 @@ GetOptions(
   'bitrate|b=s' => \$bitrate,
   'crf|q=i' => \$crf,
   'vfoptions|vf=s' => \$vf,
-  'audio|a' => \$audio,
+  'audio|a=s' => \$audio,
+  '2pass|2p:s' => \$two_pass,
   'additionalargs|ffmpegargs|cl|opts=s' => \$ffmpegargs
 );
 
 die 'No input provided.' unless $input;
-$vf = "-vf $vf" if $vf;
-$audio = '-an' ? !$audio : '';
+
+$vf = $vf ? "-vf $vf" : '';
+$audio //= '-an';
+
+if(defined($two_pass)) {
+  $two_pass ||= '/dev/null';
+  $crf = ''
+}
+else {
+  $crf = "-crf $crf"
+}
 
 # get clean filename, path, etc.
 my ($filename, $path, $suffix) = fileparse($input);
 my $input_clean = $path . $filename;
 
+my $ffprobe_out = `ffprobe -v quiet -print_format json -show_format -show_streams '$input_clean'`;
+
 # get video duration
-my $ffprobe_out = decode_json(`ffprobe -v quiet -print_format json -show_format -show_streams '$input_clean'`);
+try {
+  $ffprobe_out = decode_json($ffprobe_out)
+}
+catch {
+  die "$@:\n$ffprobe_out"
+}
+
 my $length = $stopat || int($$ffprobe_out{format}->{duration});
 
 # split and encode
 for(my $i = $offset; $i < $length; $i += $cliplen) {
   $cliplen = $length - $i if $length - $i < $cliplen;
 
-  my $ffmpegcmd = "ffmpeg -ss $i -i '$input_clean' -t $cliplen $audio -c:v libvpx -crf $crf -b:v $bitrate $vf $ffmpegargs '" . $filename . "_$i.webm'";
-  print "$ffmpegcmd:\n\n";
+  my $filename = "'" . $filename . "_$i.webm'";
 
-  `$ffmpegcmd`;
+  my $ffmpeg_cmd_start = "ffmpeg -ss $i -i '$input_clean' -t $cliplen";
+  my $ffmpeg_cmd_end = "-c:v libvpx $crf -b:v $bitrate -f webm $vf $ffmpegargs";
+
+  if($two_pass) {
+    `$ffmpeg_cmd_start -y -pass 1 -an $ffmpeg_cmd_end $two_pass`;
+    `$ffmpeg_cmd_start -pass 2 $audio $ffmpeg_cmd_end $filename`
+  }
+  else {
+    `$ffmpeg_cmd_start $audio $ffmpeg_cmd_end $filename`
+  }
 }
