1302ce898f29441ced0c99c9d90dba4d952f6580
Ian P Bradley <ian.bradley@studiocrabapple.com>
2019-09-03 17:23:40 -0700

    Seems to work but some parts could be implemented better

--- webm-splitter.pl
+++ webm-splitter.pl
@@ -19,8 +19,6 @@ my ($input, $cliplen, $offset, $duration, $bitrate, $crf, $vf, $audio, $two_pass
 # Set defaults
 $cliplen = 30;
 $offset = 0;
-# $bitrate = '1M';
-# $crf = 10;
 
 # Handle command line args
 Getopt::Long::Configure('pass_through');
@@ -30,42 +28,17 @@ GetOptions(
   'cliplength|clipduration|l=i' => \$cliplen,
   'offset|startat|o=i' => \$offset,
   'duration|stopat|d=i' => \$duration,
-  #'bitrate|b=s' => \$bitrate,
-  #'crf|q=i' => \$crf,
-  #'vfoptions|vf=s' => \$vf,
   #'audio|a=s' => \$audio,
-  '2pass|2p:s' => \$two_pass,
-  #'additionalargs|ffmpegargs|cl|opts=s' => \$ffmpegargs
+  '2pass|2p:s' => \$two_pass
 );
 
 die 'No input file provided.' unless $input;
 die 'Input file not found.' unless -e $input;
 
-#p(@ARGV);
-
-#Getopt::Long::Configure('pass_through');
 my ($ret, $ffmpegargs) = GetOptionsFromString(join(' ', @ARGV));
-p($ffmpegargs);
 
-#my $p = Getopt::Long::Parser->new( config => [ 'pass_through' ] );
-
-#my ($ret, $args) = $p->GetOptionsFromString($audio);
-
-#$vf = $vf ? "-vf $vf" : '';
-
-# Getopt::Long::Configure('pass_through');
-#my ($ret, $audio) = GetOptionsFromString($audio) if $audio;
-#my ($ret, $ffmpegargs) = GetOptionsFromString($audio) if $audio;
-
-#$audio //= '-an';
-
-if(defined($two_pass)) {
-  $two_pass ||= '/dev/null';
-  #$crf = ''
-}
-else {
-  #$crf = "-crf $crf"
-}
+$two_pass ||= '/dev/null'
+  if defined $two_pass;
 
 # Get clean filename, path, etc.
 my $file = path($input);
@@ -92,16 +65,14 @@ my $length = $duration || int($ffprobe_out->{format}{duration});
 for (my $i = $offset; $i < $length; $i += $cliplen) {
   $cliplen = $length - $i if $length - $i < $cliplen;
 
-  #my $filename = "'" . $filename . "_$i.webm'";
-
   my $outfile = path("$file\_$i.webm");
   my @args_start = (qw(ffmpeg -ss), $i, '-i', $file, '-t', $cliplen);
 
   # Not sure if $ffmpegargs will work as expected this way
-  my @args_end = (qw(-c:v libvpx -f webm));
+  my @args_end = qw(-c:v libvpx -f webm);
 
-  #push @args_end, ('-vf', $vf) if $vf;
-  #push @args_end, @ffmpegargs if scalar @ffmpegargs;
+  push @args_end, @$ffmpegargs
+    if ref $ffmpegargs eq 'ARRAY' && scalar @$ffmpegargs;
 
   if($two_pass) {
     system(@args_start, qw(-y -pass 1), @args_end, '-an', $two_pass); # Maybe I do need to store audio stuff in a variable
